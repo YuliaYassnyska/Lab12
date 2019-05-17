bc445b6258ed28999e79d16a3b223521adeef29c
Zenoviy Veres <viruslviv@gmail.com>
2019-05-17 11:25:59 +0300

    fix regex

--- Data.py
+++ Data.py
@@ -3,8 +3,9 @@ import re
 with open('access_log_Jul95', 'r') as file:
     step = 1
     for lines_of_file in file.readlines():
-        pattern = r".+01/Jul/1995:00:[0-10]:[0-60].+NASA.+"
+        pattern = r'01/Jul/1995:00:(1[0-9]).*NASA.*'
         if re.search(pattern, lines_of_file):
             step = step + 1
+            print(lines_of_file)
 print("Amount of request : " + str(step-1))
 
